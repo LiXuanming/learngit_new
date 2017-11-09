9e3b6ace7ad747434743e4264294f6a333099036
Xuanming <764121142@qq.com>
2017-11-09 10:22:12 +0800

    modified main.cpp file

--- TextQuery/main.cpp
+++ TextQuery/main.cpp
@@ -6,4 +6,56 @@ string make_plural(size_t ctr, const string &word, const string &ending)
 	return (ctr == 1) ? word : word + ending;
 }
 
-ifstream &
+ifstream& open_file(ifstream &in, const string &file)
+{
+	in.close();
+	in.clear();
+	in.open(file.c_str());
+	return in;
+
+}
+
+void print_results(const vector<TextQuery::line_no>& locs, const string& sought, const TextQuery& file)
+{
+	typedef vector<TextQuery::line_no> line_nums;
+	line_nums::size_type size = locs.size();
+	cout << "\n" << sought << "occurs" << size << " " << make_plural(size, "time", "s") << endl;
+	line_nums::const_iterator it = locs.begin();
+	for (; it != locs.end(); ++it)
+	{
+		cout << "\t(line" << (*it) + 1 << ")" << file.text_line(*it) << endl;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	ifstream infile;
+	if (argc != 2 || !open_file(infile, argv[1]))
+	{
+		cerr << "No input file!" << endl;
+		return EXIT_FAILURE;
+	}
+	TextQuery tq;
+	tq.read_file(infile);
+
+	while (true)
+	{
+		cout << "enter word to look for, or q to quit: ";
+		string s;
+		cin >> s;
+		string ret;
+		for (string::const_iterator it = s.begin(); it != s.end(); ++it)
+		{
+			ret += tolower(*it);
+		}
+		s = ret;
+		if (!cin || s == "q" || s == "Q")
+		{
+			break;
+		}
+		vector<TextQuery::line_no> locs = tq.run_query(s);
+		print_results(locs, s, tq);
+	}
+	return 0;
+}
+
